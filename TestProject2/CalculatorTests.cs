e02221009f4486fc94bc99298dc3aea7e398dffe
Tdionhogh <159053190+Tdionhogh@users.noreply.github.com>
2025-04-11 11:52:26 +0200

    første commit

---
+++ TestProject2/CalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿
+    using WpfApp1; // Husk at ændre namespace til det rigtige
+    using System;
+    using NUnit.Framework;
+    // Husk at ændre namespace til det rigtige
+
+
+    namespace TestProject2
+    {
+        [TestFixture]
+    public class CalculatorTests
+    {
+     private Calculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new Calculator(); // eller mock hvis du tester interface
+        }
+
+        [Test]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            int result = _calculator.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Divide_ByNonZero_ReturnsCorrectResult()
+        {
+            double result = _calculator.Divide(10, 2);
+            Assert.AreEqual(5.0, result);
+        }
+
+        [Test]
+        public void Divide_ByZero_ThrowsException()
+        {
+            Assert.Throws<DivideByZeroException>(() => _calculator.Divide(5, 0));
+        }
+    }
+
+}
+
---
+++ WpfApp1/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {  return a - b; }
+            
+        public int Multiply(int a, int b)
+        { return a * b; }
+        public double Divide(int a, int b)
+        { return a / b; }
+    }
+}
